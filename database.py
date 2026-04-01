cc95cd57d1570978ba7a3cbb3ea74909a43a7997
Palina Muliarchyk <muliarchikpolina@gmail.com>
2026-04-01 16:52:45 +0200

    changes db

--- database.py
+++ database.py
@@ -1,8 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("sqlite:///instance/klinika.db", echo=True)
+# Находим путь к папке, в которой лежит этот файл (database.py)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Собираем правильный путь к базе данных
+# Мы предполагаем, что папка 'instance' лежит в корне вашего проекта
+db_path = os.path.join(basedir, "instance", "klinika.db")
+
+# Используем f-строку для формирования пути
+engine = create_engine(f"sqlite:///{db_path}", echo=True)
 
 Base = declarative_base()
-
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
